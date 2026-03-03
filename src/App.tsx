0e284130aabfbb0d781761657b181464aab6502e
rajm-dv <dev.rajm8923@gmail.com>
2026-03-03 19:37:31 +0530

    added login and register page

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default App;
---
+++ src/pages/Register.tsx
@@ -0,0 +1,66 @@
+import { Link } from "react-router-dom";
+
+const Register = () => {
+  return (
+    <div className="min-h-screen grid grid-cols-12 text-black/80">
+      <div className="col-span-7 p-10">
+        <div>
+          <img className="h-12 mx-10" src="./logo.png" alt="logo" />
+        </div>
+        <div className="my-10 flex justify-center">
+          <div className="">
+            <h1 className="text-3xl font-semibold">
+              Register and start sharing
+            </h1>
+            <p className="my-2">
+              Already have an account?{" "}
+              <Link
+                to={"/login"}
+                className="text-blue-500 hover:underline hover:text-blue-600"
+              >
+                Log in
+              </Link>
+            </p>
+            <div className="my-8">
+              <p className="font-semibold my-2">Username</p>
+              <input
+                name="username"
+                type="text"
+                className="px-3 border border-slate-300 h-12 w-full"
+              />
+              <p className="font-semibold my-2 pt-3">Email</p>
+              <input
+                name="email"
+                type="emain"
+                className="px-3 border border-slate-300 h-12 w-full"
+              />
+              <p className="font-semibold my-2 pt-3">Password</p>
+              <input
+                name="password"
+                type="password"
+                className="px-3 border border-slate-300 h-12 w-full"
+              />
+              <p className="font-semibold my-2 pt-3">Confirm Password</p>
+              <input
+                name="password"
+                type="password"
+                className="px-3 border border-slate-300 h-12 w-full"
+              />
+              <button className="my-8 bg-blue-600 hover:bg-blue-700 w-full p-2 text-lg font-medium text-white rounded">
+                Register
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-span-5 flex flex-col justify-center items-center bg-slate-100">
+        <img src="./user1.svg" alt="user" />
+        <p className="font-semibold my-5">
+          Connect Shortfox to the tools you use every day
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
