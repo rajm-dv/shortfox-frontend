37df448a8e3324551d36b3d3a6c11ef8e2dfd8ce
rajm-dv <dev.rajm8923@gmail.com>
2026-03-07 22:45:18 +0530

    added sticky sidebar at dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,10 +3,20 @@ import Navbar from "../components/Navbar";
 const Dashboard = () => {
   return (
     <div className="grid grid-cols-12">
-      <div className="col-span-2 bg-red-500">
-        <div className="flex px-2"></div>
+      <div className="col-span-3 bg-slate-300 h-dvh sticky top-0">
+        <div className="flex p-5">
+          <div className="flex items-center">
+            <div className="bg-blue-500 rounded-full size-12 flex items-center justify-center p-4 text-3xl font-semibold text-white">
+              R
+            </div>
+            <div className="ml-3">
+              <p className="font-semibold italic">@rajm-dv</p>
+              <p className="text-sm">rajmanna@gmail.com</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="col-span-10 pr-20">
+      <div className="col-span-9 pl-5 pr-10">
         <Navbar />
       </div>
     </div>
