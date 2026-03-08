2f940fec1136f07a33924db52fa3fecb70c29347
rajm-dv <dev.rajm8923@gmail.com>
2026-03-08 22:07:57 +0530

    added table /dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,4 @@
+import { Cross, Trash2, X } from "lucide-react";
 import Navbar from "../components/Navbar";
 
 const Dashboard = () => {
@@ -36,6 +37,54 @@ const Dashboard = () => {
       </div>
       <div className="col-span-9 px-8">
         <Navbar />
+        <div className="grid grid-cols-3 my-2">
+          <div className="bg-blue-500 p-5">
+            <p className="text-lg">Total links</p>
+            <p className="text-2xl font-medium">200</p>
+          </div>
+          <div className="bg-orange-500 p-5">
+            <p className="text-lg">Total Clicks</p>
+            <p className="text-2xl font-medium">4000</p>
+          </div>
+          <div className="bg-red-500 p-5">
+            <p className="text-lg">Dead links</p>
+            <p className="text-2xl font-medium">10</p>
+          </div>
+        </div>
+        <table className="table-auto w-full my-10">
+          <thead className="bg-slate-200">
+            <tr className="h-12">
+              <th>Serial</th>
+              <th>Origin</th>
+              <th>Redirection</th>
+              <th>Dead Status</th>
+              <th>Details</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-slate-100 h-12">
+              <th>1</th>
+              <th>https://gogole.com</th>
+              <th>https://abs.com</th>
+              <th>No</th>
+              <th>View</th>
+              <th className="flex justify-center items-center h-12">
+                <Trash2 className="size-5 text-red-500" />
+              </th>
+            </tr>
+            <tr className="bg-slate-50 h-12">
+              <th>2</th>
+              <th>https://gogole.com</th>
+              <th>https://abs.com</th>
+              <th>No</th>
+              <th>View</th>
+              <th className="flex justify-center items-center h-12">
+                <Trash2 className="size-5 text-red-500" />
+              </th>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
