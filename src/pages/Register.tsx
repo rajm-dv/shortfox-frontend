9c6c52c08cf04cf3c9f5265d12e9e1108bc1cd4c
rajm-dv <dev.rajm8923@gmail.com>
2026-03-21 21:20:43 +0530

    added register store

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,9 +1,53 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 import SideImageComponent from "../components/SideImageComponent";
 import AppLogoComponent from "../components/AppLogoComponent";
+import useAuthStore from "../store/auth";
+import toast from "react-hot-toast";
 
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cPassword, setCPassword] = useState("");
+
+  const { register, isLoading, isAuthenticated, error } = useAuthStore();
+  const navigate = useNavigate();
+
+  const handleRegister = async (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!username || !email || !password || !cPassword) {
+      toast.error("Enter credientials");
+      return;
+    }
+
+    if (password !== cPassword) {
+      toast.error("Password should be same");
+      return;
+    }
+    const { success } = await register({
+      username: username.trim(),
+      email: email.trim(),
+      password: password.trim(),
+      confirmPassword: cPassword.trim(),
+    });
+    if (success) toast.success("Register successfully");
+  };
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/", { replace: true });
+    }
+  }, [isAuthenticated, navigate]);
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+    }
+  }, [error]);
+
   return (
     <div className="min-h-screen grid grid-cols-12 text-black/80">
       <div className="col-span-7 p-10">
@@ -24,35 +68,51 @@ const Register = () => {
                 Log in
               </Link>
             </p>
-            <div className="my-8">
+            <form onSubmit={handleRegister} className="my-8">
               <p className="font-semibold my-2">Username</p>
               <input
                 name="username"
                 type="text"
+                onChange={(e) => setUsername(e.target.value)}
+                value={username}
                 className="px-3 border border-slate-300 h-12 w-full"
+                required
               />
               <p className="font-semibold my-2 pt-3">Email</p>
               <input
                 name="email"
-                type="emain"
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
                 className="px-3 border border-slate-300 h-12 w-full"
+                required
               />
               <p className="font-semibold my-2 pt-3">Password</p>
               <input
                 name="password"
                 type="password"
+                onChange={(e) => setPassword(e.target.value)}
                 className="px-3 border border-slate-300 h-12 w-full"
+                required
               />
               <p className="font-semibold my-2 pt-3">Confirm Password</p>
               <input
                 name="password"
                 type="password"
+                onChange={(e) => setCPassword(e.target.value)}
                 className="px-3 border border-slate-300 h-12 w-full"
+                required
               />
-              <button className="my-8 bg-blue-600 hover:bg-blue-700 w-full p-2 text-lg font-medium text-white rounded">
+              <button
+                disabled={
+                  isLoading || !username || !email || !password || !cPassword
+                }
+                type="submit"
+                className="my-8 bg-blue-600 hover:bg-blue-700 w-full p-2 text-lg font-medium text-white rounded disabled:bg-blue-400"
+              >
                 Register
               </button>
-            </div>
+            </form>
           </div>
         </div>
       </div>
--- src/store/auth.ts
+++ src/store/auth.ts
@@ -13,6 +13,12 @@ interface AuthState {
   isLoading: boolean;
   isAuthenticated: boolean;
 
+  register: (data: {
+    username: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+  }) => Promise<Success>;
   login: (data: { email: string; password: string }) => Promise<Success>;
   logout: () => void;
 }
@@ -27,6 +33,40 @@ const useAuthStore = create<AuthState>()(
       error: null,
       isLoading: false,
       isAuthenticated: false,
+
+      // register
+      register: async ({ username, email, password, confirmPassword }) => {
+        set({ isLoading: true, error: null });
+        try {
+          const res = await axios.post(`${API_URL}/api/v1/auth/register`, {
+            username,
+            email,
+            password,
+            confirmPassword,
+          });
+          const { token, user } = res.data;
+          set({
+            token,
+            user,
+            isAuthenticated: true,
+            isLoading: false,
+            error: null,
+          });
+          return { success: true };
+        } catch (err: unknown) {
+          let message = "Registation failed";
+          if (axios.isAxiosError(err)) {
+            message = err.message || err.response?.data?.message;
+          }
+          set({
+            error: message,
+            isAuthenticated: false,
+            isLoading: false,
+            token: null,
+          });
+          return { success: false };
+        }
+      },
 
       // login
       login: async ({ email, password }) => {
